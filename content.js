11250a1e4a197714bc09728ae6f7fdd6bfe31208
Amidou Kante <kamdiou@gmail.com>
2019-09-19 14:25:21 -0400

    add variable to track ad keys

--- content.js
+++ content.js
@@ -4,12 +4,14 @@ window.onload = () => {
   trackNewContent(body);
 };
 
+const searchTerm = "div[data-author='Triplebyte_official']";
+
 const trackNewContent = (body) => {
   const resizeObserver = new ResizeObserver(removeTripleByteAds);
   resizeObserver.observe(body);
 }
 
 const removeTripleByteAds = () => {
-  const tripleByteAds = document.querySelectorAll("div[data-author='Triplebyte_official']");
+  const tripleByteAds = document.querySelectorAll(searchTerm);
   tripleByteAds.forEach((ad) => ad.remove());
 }
